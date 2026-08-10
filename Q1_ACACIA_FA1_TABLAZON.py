df65e4491d6e254f221086ed21eb83e764bc3fc6
zhy-mi <zmctablazon31@src.pshs.edu.ph>
2026-08-10 09:44:54 +0800

    Add files via upload

---
+++ Q1_ACACIA_FA1_TABLAZON.py
@@ -0,0 +1,21 @@
+import math
+
+#input variables
+x1 = float(input("Enter x1: "))
+x2 = float(input("Enter x2: "))
+y1 = float(input("Enter y1: "))
+y2 = float(input("Enter y2: "))
+
+#distance formula
+distance = math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2))
+
+#answer
+print(f"\n The distance between the two points is : {distance:.2f}")
+
+
+
+"""
+Using library is more practical because it helped simplifying my program instead of typing long formulas and made 
+calculating easier by providing math.sqrt and math.pow. Without it, writing the program would be more difficult because 
+i would have to rely on intuitive syntax.
+"""
